2598d27291c6daaec9824e824df406df7a7314b1
race-briggs <race.briggs@gmail.com>
2019-09-16 09:57:13 -0500

    added a hidden form for the questions

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -3,7 +3,18 @@ $(document).ready(function(){
 
   var numCorrect = 0;
 
+  var questionTracker = 0;
+
+  
   $('.start').on('click', function(){
     $('.start').attr('class', 'hidden')
+
+    $('.question-div').removeClass('hidden');
+
+    startGame();
   })
+
+  function startGame() {
+
+  }
 })
