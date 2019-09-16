6287a571957ed4f9fae0c585c8463137cd39e97a
race-briggs <race.briggs@gmail.com>
2019-09-16 10:09:34 -0500

    added first question object and established a wireframe

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,11 +1,49 @@
 $(document).ready(function(){
-  var questions = [];
+  var questions = [
+  {
+    questionNumber: 1,
+    questionText: "Who is the current leader of the Horde?",
+    answers: [{
+      answerNumber: 1,
+      answerText: 'Thrall',
+      answerCorrect: false
+    },
+    {
+      answerNumber: 2,
+      answerText: 'Sylvanas',
+      answerCorrect: true
+    },
+    {
+      answerNumber: 3,
+      answerText: "Vol'jin",
+      answerCorrect: false
+    },
+    {
+      answerNumber: 1,
+      answerText: 'Baine',
+      answerCorrect: false
+    }]
+  }, 
+  {
+
+  }, 
+  {
+
+  }, 
+  {
+
+  },
+  {
+
+  }];
 
   var numCorrect = 0;
 
+  var numQuestions = questions.length;
+
   var questionTracker = 0;
 
-  
+
   $('.start').on('click', function(){
     $('.start').attr('class', 'hidden')
 
